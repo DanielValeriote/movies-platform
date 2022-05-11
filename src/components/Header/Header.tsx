29f75f3ba29d870f20e37f42682b052e4bc6ff36
DanielValeriote <danielvaleriote@outlook.com>
2022-05-11 14:07:29 -0300

    add back to home Link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,10 @@
+import { Link } from 'react-router-dom'
 import './Header.scss'
 
 export const Header = ({}) => {
 	return <header className="page-header">
-		<h1>Home</h1>
+		<Link to="/">
+			<h1>Home</h1>
+		</Link>
 	</header>
 }
