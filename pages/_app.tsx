68424d9dcd99f42168ac31365c3f7592b10dce78
DanielValeriote <danielvaleriote@outlook.com>
2022-06-15 21:27:18 -0300

    add progress bar with NProgress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Navbar } from '../components';
+
+// Imports for StyledComponents theme 
 import { ThemeProvider } from 'styled-components';
 import theme from '../styles/theme';
-import { Navbar } from '../components';
+
+// Imports for progress bar
+import 'nprogress/nprogress.css';
+import NProgress from 'nprogress';
+import Router from 'next/router';
+
+const CustomNProgress = NProgress.configure({ showSpinner: false });
+
+Router.events.on("routeChangeStart", () => CustomNProgress.start());
+Router.events.on("routeChangeComplete", () => CustomNProgress.done());
+Router.events.on("routeChangeError", () => CustomNProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <ThemeProvider theme={theme}>
