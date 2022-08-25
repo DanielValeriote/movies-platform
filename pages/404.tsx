7b424a41025ab2bf32814cec33f487a6950f5acc
DanielValeriote <danielvaleriote@outlook.com>
2022-08-24 21:21:52 -0300

    create not found page and redirects when movie query is unsuccessful.

---
+++ pages/404.tsx
@@ -0,0 +1,37 @@
+import { NextPage } from "next";
+import Link from "next/link";
+
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  padding-top: 90px;
+  flex-direction: column;
+  text-align: center;
+  gap: 25px;
+
+  & a {
+    color: ${(props) => props.theme.colors.mainBlue};
+    font-weight: bold;
+  }
+`;
+
+const NotFoundMessage = styled.h1`
+  padding-top: 80px;
+  text-align: center;
+  font-weight: 600;
+`;
+
+const NotFound: NextPage = () => {
+  return (
+    <Container>
+      <NotFoundMessage>Página não encontrada.</NotFoundMessage>
+      <Link href="/" passHref>
+        <a>Voltar ao início</a>
+      </Link>
+    </Container>
+  );
+};
+
+export default NotFound;
--- pages/movie/[id].tsx
+++ pages/movie/[id].tsx
@@ -5,50 +5,69 @@ import { fetchSingleMovie } from "../../utils/fetchSingleMovie";
 import { DetailedMovieCard } from "../../components";
 import styled from "styled-components";
 
-type uncertainMovie = DetailedMovieData | undefined;
+interface errorResponse {
+  success: boolean;
+  status_code: number;
+  status_message: string;
+}
 
-interface Props { movie: uncertainMovie }
+type uncertainResponse = DetailedMovieData | undefined | errorResponse;
 
-export const getStaticPaths: GetStaticPaths = async () => (
-	{
-		paths: [{ params: {id: "499932"} }],
-		fallback: true
-	}
-);
+interface Props {
+  movie: DetailedMovieData;
+}
+
+export const getStaticPaths: GetStaticPaths = async () => ({
+  paths: [{ params: { id: "499932" } }],
+  fallback: true,
+});
 
 export const getStaticProps: GetStaticProps = async (context) => {
-	const id = context.params?.id;
-	let movie: uncertainMovie = undefined;
-	if(id && typeof(id) === "string") movie = await fetchSingleMovie(id);
+  const id = context.params?.id;
+  let movie: uncertainResponse = undefined;
+  if (id && typeof id === "string") movie = await fetchSingleMovie(id);
 
-	return {
-		props: {
-			movie
-		}
-	};
+  // verifies if the response object is of type movie or error
+  if (!movie || "success" in movie) {
+    return {
+      redirect: {
+        destination: "/movie_not_found",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      movie,
+    },
+  };
 };
 
 export const Container = styled.div`
-	width: 80%;
-	margin: 20px auto;
+  width: 80%;
+  margin: 20px auto;
 
-	@media screen and (max-width: 530px) {
-		& {
-			width: 100%;
-		}
-	}
+  @media screen and (max-width: 530px) {
+    & {
+      width: 100%;
+    }
+  }
 `;
 
-const Movie = ({movie}: Props) => {
-	return <>
-		<Head>
-			<title>{movie?.title || "Filme não encontrado"}</title>
-			<meta name="description" content={`Detalhes do filme${" '" + movie?.title + "'" || "."}`}/>
-		</Head>
-		<Container>
-			{ movie && <DetailedMovieCard movieData={movie} /> }
-		</Container>
-	</>;
+const Movie = ({ movie }: Props) => {
+  return (
+    <>
+      <Head>
+        <title>{movie?.title || "Filme não encontrado"}</title>
+        <meta
+          name="description"
+          content={`Detalhes do filme${" '" + movie?.title + "'" || "."}`}
+        />
+      </Head>
+      <Container>{movie && <DetailedMovieCard movieData={movie} />}</Container>
+    </>
+  );
 };
 
 export default Movie;
